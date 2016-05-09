4133c2468b8da5a9bb05dfa1614e7b38e2e30004
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2016-05-09 08:10:19 +0100

    clase09052016

---
+++ mainfromfile.js
@@ -0,0 +1,36 @@
+#!/usr/local/bin/node --harmony_destructuring
+"use strict";
+const process = require('process');
+/* See gist: https://gist.github.com/branneman/8048520 
+ * Better local require() paths for Node.js */
+process.env.NODE_PATH += ":"+__dirname+"/lib/";
+require('module').Module._initPaths();
+var util = require('util');
+var fs = require('fs');
+var PEG = require("pl0node.js");
+var fileName = process.argv[2] || 'tests/input4.pl0';
+var semantic = require("semantic");
+
+const lineNumbers = (input) => {
+  let count = 1;
+  return input.replace(/^/mg, (x) => { 
+      let str = ' '+count++;
+      return (str.substr(str.length-2)+' ')
+    });
+};
+
+fs.readFile(fileName, 'utf8', function (err,input) {
+  if (err) {
+    return console.log(err);
+  }
+  console.log(`Processing <***\n${lineNumbers(input)}\n***>`);
+  try {
+    var r = PEG.parse(input);
+    semantic(r);
+    console.log(util.inspect(r, {depth: null}));
+  } catch (e) {
+    //console.log(`Error en línea ${e.location.start.line} columna ${e.location.start.column}`);
+    console.log(e);
+  }
+});
+
